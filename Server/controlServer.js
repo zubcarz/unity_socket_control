9f084210b45c6dc0b475c6bc755ef204ab40bfaa
Carlos Zubieta <zubcarz@gmail.com>
2018-07-09 13:09:43 -0500

    Change port

--- Server/controlServer.js
+++ Server/controlServer.js
@@ -6,7 +6,7 @@ var io = require('socket.io')({
 
 // Setting
 io.attach(4567);
-gpio.setup(7, gpio.DIR_OUT, write);
+gpio.setup(21, gpio.DIR_OUT, write);
 var isEmiting = true;
 
 // Sockets
@@ -15,14 +15,14 @@ io.on('connection', function(socket){
 	socket.on('ready', function(){
 		socket.emit('button1');
 		isEmiting = (!isEmiting) ? true : false;
-		gpiop.write(7, isEmiting);
+		gpiop.write(21, isEmiting);
 	});
 
 })
 
 function write(err) {
     if (err) throw err;
-    gpio.write(7, true, function(err) {
+    gpio.write(21, true, function(err) {
         if (err) throw err;
         console.log('Written to pin');
     });
