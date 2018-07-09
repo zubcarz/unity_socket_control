7975139b34f69595499f6cd93cbdd2786eaff0fd
Carlos Zubieta <zubcarz@gmail.com>
2018-07-09 17:03:18 -0500

    New GPIO

--- Server/controlServer.js
+++ Server/controlServer.js
@@ -1,29 +1,21 @@
 // Requires
-var gpio = require('rpi-gpio');
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
 var io = require('socket.io')({
 	transports: ['websocket'],
 });
 
 // Setting
 io.attach(4567);
-gpio.setup(21, gpio.DIR_OUT, write);
-var isEmiting = true;
+var isEmiting = 1;
 
 // Sockets
 io.on('connection', function(socket){
 
 	socket.on('ready', function(){
 		socket.emit('button1');
-		isEmiting = (!isEmiting) ? true : false;
-		gpiop.write(21, isEmiting);
+		isEmiting = (!isEmiting) ? 1 : 0;
+		LED.writeSync(isEmiting);
 	});
 
 })
-
-function write(err) {
-    if (err) throw err;
-    gpio.write(21, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
-}
