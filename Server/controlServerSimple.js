85de36235d7f6e39b32ecba270ada0749ebbb1d8
Carlos Zubieta <zubcarz@gmail.com>
2018-07-09 12:46:57 -0500

    Simple server

--- Server/controlServerSimple.js
+++ Server/controlServerSimple.js
@@ -1,21 +1,18 @@
 // Requires
-var gpio = require('rpi-gpio');
+
 var io = require('socket.io')({
 	transports: ['websocket'],
 });
 
 // Setting
 io.attach(4567);
-gpio.setup(7, gpio.DIR_OUT, write);
-var isEmiting = true;
+
 
 // Sockets
 io.on('connection', function(socket){
 
 	socket.on('ready', function(){
 		socket.emit('button1');
-		isEmiting = (!isEmiting) ? true : false;
-		gpiop.write(7, isEmiting);
 	});
 
 })
