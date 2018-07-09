497f3a8b1c24e83054f072aa899f99c04c5959e2
Carlos Zubieta <zubcarz@gmail.com>
2018-07-09 12:34:21 -0500

    Gpio

--- Server/controlServer.js
+++ Server/controlServer.js
@@ -1,13 +1,21 @@
+// Requires
+var gpio = require('rpi-gpio');
 var io = require('socket.io')({
 	transports: ['websocket'],
 });
 
+// Setting
 io.attach(4567);
+gpio.setup(7, gpio.DIR_OUT, write);
+var isEmiting = true;
 
+// Sockets
 io.on('connection', function(socket){
 
-	socket.on('ready', function(){
+	socket.on('ready', function()
 		socket.emit('button1');
+		isEmiting = (!isEmiting) ? true : false;
+		gpiop.write(7, isEmiting);
 	});
 
 })
